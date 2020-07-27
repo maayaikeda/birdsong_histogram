e38d5125209858181636223cdb1187cf0c803704
maayaikeda <68711322+maayaikeda@users.noreply.github.com>
2020-07-27 11:41:54 +0000

    Update song_context_histogram.R

--- song_context_histogram.R
+++ song_context_histogram.R
@@ -1,27 +1,141 @@
+# import readxl to read excel sheets
 library(readxl)
-G544_mMAN <- read_excel("G544_mMAN.xlsx")
-data <- G544_mMAN
+# import data from different birds
+birdsong_1 <- read_excel("~/Documents/Home 2020/birdsong_1.xlsx")
+birdsong_2 <- read_excel("~/Documents/Home 2020/birdsong_2.xls")
+# check to see if they look ok
+head(birdsong_1)
+head(birdsong_1)
+# combine the data
+data <- rbind(birdsong_1, birdsong_2)
+# check dafa
 head(data)
+# clean up data; get rid of durations that are too short to be songs 
 new_data<- data[400 < data$motif_duration,]
+
+# Songs while young are begging can be considered "undirected song" 
+# so relabel them as male singing alone
+new_data[new_data == "alone"]<-"Male singing alone"
+new_data[new_data == "interact"]<-"Mate responding to song"
+new_data[new_data == "beg"]<-"Male singing alone"
+# plot data and see how it looks
+p <- ggplot(new_data, aes(motif_duration,fill=context2, color=birdID)) + geom_histogram(binwidth = 2, , alpha = 0.5, position = "identity") + ggtitle("Motif duration: context")+ xlab("Motif duration") +labs(fill = "Sounds in Recording")
+p <- p + scale_fill_brewer(palette="Set2")
+# get rid of grid
+p <- p + theme_classic()
+# label y axis
+p <- p + ylab("Count")
+p
+
+# It seems like directed songs are shorter in length than undirected songs
+# so combine the birds and normalize it to their undirected (alone) mean. 
+# (substract all points from undirected mean)
+
+# Count number of rows 
+rows = 1:length(new_data$motif_duration)
+# Create empty column
+new_data[,"diff_frm_mean"] <- NA
+# Make sure it looks ok
+head(new_data)
+
+# Need this library to summarize data. Load it.
+library(Rmisc)
+# Summary table. Call it df1.
+df1 <- summarySE(new_data, measurevar = "motif_duration", groupvars=c("birdID", "context2"))
+# Take a look at df1
+df1
+# Extract means for undirected (alone) songs
+G544undir_mean <- df1[1, 4]
+O375undir_mean <- df1[3, 4]
+
+
+# Add motif_duration-mean values in the new column. 
+# If the bird is G544, substract G544 undirected mean, otherwise, substract O375's undirected mean.
+for (r in rows){
+  if (new_data$birdID[r] == "G544"){
+    new_data$diff_frm_mean[r] <- new_data$motif_duration[r]-G544undir_mean
+    } else { 
+      new_data$diff_frm_mean[r] <- new_data$motif_duration[r]-O375undir_mean
+  }
+}
+
+# Check if it looks ok
+head(new_data$diff_frm_mean)
+head(new_data$motif_duration)
+
+# Plot the data
 library(ggplot2)
-p <- ggplot(new_data, aes(motif_duration,color=comments)) + geom_histogram(fill = "white",position = "dodge") + ggtitle("motif length")
-p <- p + scale_color_manual(values=c("pink1","pink3","green1", "green3" ))
-#p <- p +theme(legend.position = "none")
+p <- ggplot(new_data, aes(diff_frm_mean,color=context2)) + geom_histogram(fill = "white",position = "dodge") + ggtitle("Motif length")
+p <- p + labs(color = "Context")
 p <- p + theme_classic()
 p
+
+# Another way to summarize mean.
 library(plyr)
-mu <- ddply(new_data, "comments", summarise, grp.mean=mean(motif_duration))
-head(mu)
-ggplot(new_data, aes(x=motif_duration, color=comments, fill=comments)) +
-  geom_histogram(aes(y=..density..), position="identity", alpha=0.5)+
-  geom_density(alpha=0.6)+
-  geom_vline(data=mu, aes(xintercept=grp.mean, color=comments),
-             linetype="dashed")+
-  scale_color_manual(values=c("pink1","pink3","darkseagreen1", "darkseagreen3"),name="Experimental\nCondition",
-                     breaks=c("post", "postfemale", "pre","prefemale"),
-                     labels=c("Post surgery undirected", "Post surgery directed", "Pre surgery undirected", "Pre surgery directed"))+
-  scale_fill_manual(values=c("pink1","pink3","darkseagreen1", "darkseagreen3"),name="Experimental\nCondition",
-                    breaks=c("post", "postfemale", "pre","prefemale"),
-                    labels=c("Post surgery undirected", "Post surgery directed", "Pre surgery undirected", "Pre surgery directed"))+
-  labs(title="Motif Length", y = "Density")+
-  theme_classic()
+mu2 <- ddply(new_data, "context2", summarise, grp.mean=mean(diff_frm_mean))
+head(mu2)
+
+# Plot density
+p <- ggplot(new_data, aes(x=diff_frm_mean, color=context2, fill=context2))
+p <- p + geom_histogram(aes(y=..density..), position="identity", alpha=0.5) 
+p <- p + geom_density(alpha=0.5)
+p <- p + geom_vline(data=mu2, aes(xintercept=grp.mean, color=context2),linetype="dashed")
+p <- p + scale_color_manual(values=c("pink3", "darkseagreen3"),name="Experimental\nCondition",
+                            breaks=c("Male singing alone", "Mate responding to song"),
+                     labels=c("Undirected", "Directed"))
+p <- p + scale_fill_manual(values=c("pink3", "darkseagreen3"),name="Experimental\nCondition",
+                    breaks=c("Male singing alone", "Mate responding to song"),
+                    labels=c("Undirected", "Directed"))
+p <- p + labs(title="Motif Length", y = "Density", x="Motif length difference from directed mean")
+p <- p + theme_classic()
+p
+
+# Plot to see how it looks for each bird.
+
+# It is known from previous studies that directed songs are shorter than undirected songs. 
+# I think we can assume that "Male singing alone" is "undirected" song 
+# and "Mate responding to song" is actually is a female call contaminated recording.
+# So relabel them. 
+
+new_data[,"contexts"] <- NA
+for (r in rows){
+  if (new_data$birdID[r] == "G544" && new_data$context2[r] == "Male singing alone"){
+    new_data$contexts[r] <- "Undirected G544"
+  } else if (new_data$birdID[r] == "G544" && new_data$context2[r] == "Mate responding to song"){
+    new_data$contexts[r] <- "Female call contaminated recording G544"
+  } else if (new_data$birdID[r] == "O375" && new_data$context2[r] == "Mate responding to song"){
+    new_data$contexts[r] <- "Female call contaminated recording O375"
+  } else { 
+    new_data$contexts[r] <- "Undirected O375"
+  }
+}
+
+
+# Get a stats summary for density plot.
+df2 <- summarySE(new_data, measurevar = "diff_frm_mean", groupvars=c("birdID", "contexts"))
+df2
+
+# Density plot for each bird. Pink = G544, Green = O375; lighter color = undirected
+# lighdarker color = directed
+p <- ggplot(new_data, aes(x=diff_frm_mean, color=contexts, fill=contexts))
+p <- p + geom_histogram(aes(y=..density..), position="identity", alpha=0.5) 
+p <- p + geom_density(alpha=0.5)
+p <- p + geom_vline(data=df2, aes(xintercept=diff_frm_mean, color=contexts),linetype="dashed")
+p <- p + scale_color_manual(values = c("pink3", "pink1", "darkseagreen3", "darkseagreen1"), name = "Context/Bird",
+                            breaks = c("Female call contaminated recording G544", "Undirected G544", "Female call contaminated recording O375", "Undirected O375"),
+                            labels = c("Contaminated recording G544", "Undirected G544", "Contaminated recording O375", "Undirected O375"))
+p <- p + scale_fill_manual(values = c("pink3", "pink1", "darkseagreen3", "darkseagreen1"), name = "Context/Bird",
+                            breaks = c("Female call contaminated recording G544", "Undirected G544", "Female call contaminated recording O375", "Undirected O375"),
+                            labels = c("Contaminated recording G544", "Undirected G544","Contaminated recording O375", "Undirected O375"))
+p <- p + labs(title="Undirected vs Directed in birds G544 and O375", y = "Density", x="Motif length difference from directed mean")
+p <- p + annotate("segment", x = 0, y = 0, xend = 0, yend =0.065 )
+p <- p + annotate("text", x = 5,y = 0.07, label = "Undirected mean" )
+p <- p + theme_classic()
+p
+
+
+# Conclusion, for both birds, "directed" songs or when the song files are contaminated with female
+# calls, the songs are faster.
+# Maybe looking at female call contamination in files is a way to find "directed" songs.
+
+
